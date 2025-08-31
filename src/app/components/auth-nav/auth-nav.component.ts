622b184c91b9ccd78e08b63812c07ad7a1f8c3a1
CODELABSYS\Ahmed.ElBakrey <ahmed.elbakrey@codelabsys.com>
2025-09-01 00:08:15 +0300

    (master) -> adding login page

--- src/app/components/auth-nav/auth-nav.component.ts
+++ src/app/components/auth-nav/auth-nav.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 @Component({
   selector: 'app-auth-nav',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule, RouterLink, RouterLinkActive],
   templateUrl: './auth-nav.component.html',
   styleUrl: './auth-nav.component.scss'
 })
 export class AuthNavComponent {
-
+ 
 }
